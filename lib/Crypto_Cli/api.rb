a653c1d4cc51075bcbf64d604920266fa2e15f34
eulis01 <eulis01@gmail.com>
2020-06-12 13:17:16 -0400

    refactor code

--- lib/Crypto_Cli/api.rb
+++ lib/Crypto_Cli/api.rb
@@ -1,29 +1,20 @@
 # frozen_string_literal: true
 
 class Api
-  attr_accessor :data
-  def initialize
-    @data = []
-    get_ticker
-  end
-
   KEY = ENV['API_KEY']
-  URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=#{KEY}&limit=15&cryptocurrency_type=all"
+  BASE_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=#{KEY}&limit=15&cryptocurrency_type=all"
 
-  def get_ticker
-    data_url = URL
+  def self.get_ticker
+    data_url = BASE_URL
     uri = URI(data_url)
     response = Net::HTTP.get_response(uri)
-    body = response.body
-    @data = JSON.parse(body)
+    parsed_data = JSON.parse(response.body)
+    parsed_data['data'].each do |ticker|
+      cmc_rank = ticker['cmc_rank']
+      name = ticker['name']
+      symbol = ticker['symbol']
+      price = ticker['quote']['USD']['price']
+      Ticker.new(cmc_rank, name, symbol, price)
+    end
   end
 end
-# "data": [
-#  {
-#  "id": 1,
-#  "name": "Bitcoin",
-#  "symbol": "BTC",
-#  "cmc_rank": 1,
-#  "quote": {
-#  "USD": {
-#  "price": 9356.24564637,
